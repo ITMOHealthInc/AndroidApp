83d2f7afefb50662579544ae0102f5c6403fb014
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2025-03-18 17:52:57 +0300

    chore: :wrench: some buttons idk

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -29,20 +33,43 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(modifier = Modifier.padding(innerPadding)) {
                         Greeting(name = "Android")
-                        SendRequestComposable("https://data-api.oxilor.com/rest/countries")
+//                        SendRequestComposable("https://data-api.oxilor.com/rest/countries")
                     }
                 }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Какое-нибудь другое слово $name!",
+//        fontSize = 100.sp,
+//        lineHeight = 116.sp,
         modifier = modifier
     )
+
+    Button(onClick = { onClickBreakfast() }) {
+        Text("Завтрак")
+    }
+    Button(onClick = { onClick() }) {
+        Text("Обед")
+    }
+    Button(onClick = { onClick() }) {
+        Text("Ужин")
+    }
+    Button(onClick = { onClick() }) {
+        Text("Перекус")
+    }
+}
+
+fun onClick() {
+    Toast.makeText(this, "Its pasta!", Toast.LENGTH_SHORT).show();
+}
+
+fun onClickBreakfast() {
+    Toast.makeText(this, "Its toast!", Toast.LENGTH_SHORT).show();
 }
 
 @Preview(showBackground = true)
@@ -60,7 +87,7 @@ fun SendRequestComposable(domain: String) {
     LaunchedEffect(Unit) {
         withContext(Dispatchers.IO) {
             try {
-                val API_KEY = "-5inGwyK30fxMOSmzNIz5Qntf8CRF6"
+                val API_KEY = ""
                 val client = OkHttpClient()
                 val request = okhttp3.Request.Builder()
                     .addHeader("Authorization", "Bearer $API_KEY")
@@ -80,4 +107,4 @@ fun SendRequestComposable(domain: String) {
         }
     }
     Text(text = responseText.value)
-}
+}}
