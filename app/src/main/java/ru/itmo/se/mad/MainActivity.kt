825c5d3f8da49da38c0486645eed9b7462485c96
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2025-04-17 14:41:39 +0300

    feat: :sparkles: add color list

---
+++ app/src/main/java/ru/itmo/se/mad/MainActivity.kt
@@ -0,0 +1,39 @@
+package ru.itmo.se.mad
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import ru.itmo.se.mad.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            Greeting("Sean Combs")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Sean Combs")
+    }
+}
---
+++ app/src/main/java/ru/itmo/se/mad/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package ru.itmo.se.mad.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WaterBlue = Color(0xFF019EFF)
+val WaterBlue70 = Color(0xFF84CAFE)
+val WaterBlue40 = Color(0x66019EFF)
+val WaterBlue10 = Color(0x2601B9FF)
+
+val WidgetGray80 = Color(0xCC000000)
+val WidgetGray70 = Color(0xB2000000)
+val WidgetGray60 = Color(0x99000000)
+val WidgetGray45 = Color(0xFF454545)
+val WidgetGray10 = Color(0x0A000000)
+val WidgetGray5 = Color(0x05000000)
+val WidgetGray3 = Color(0x03000000)
+
+val BackgroundGray4560 = Color(0x99454545)
+val BackgroundGray14 = Color(0xFF141414)
+val BackgroundGray53 = Color(0xFF535353)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val CalorieGreen = Color(0xFF00E30B)
+val CalorieGreenMenu = Color(0xFF52D500)
+val CalorieGreen30 = Color(0x4D00E30B)
+val CalorieGreen15 = Color(0x2600E30B)
+
+
+val ActivityOrange85 = Color(0xD9FF8401)
+val ActivityOrange15 = Color(0x26FF8401)
+val CalorieOverflow = Color(0xFFE35F00)
