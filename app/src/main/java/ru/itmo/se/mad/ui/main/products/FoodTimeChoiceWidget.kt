21fc6a5154e408af14f8a85f0c35b09259dabb8d
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2025-04-20 22:05:47 +0300

    bug: :bug: optimize imports

--- app/src/main/java/ru/itmo/se/mad/ui/main/products/FoodTimeChoiceWidget.kt
+++ app/src/main/java/ru/itmo/se/mad/ui/main/products/FoodTimeChoiceWidget.kt
@@ -1,19 +1,16 @@
 package ru.itmo.se.mad.ui.main.products
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -28,9 +25,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,8 +35,6 @@ import ru.itmo.se.mad.ui.theme.WaterBlue
 import ru.itmo.se.mad.ui.theme.WaterBlue10
 import ru.itmo.se.mad.ui.theme.White
 import ru.itmo.se.mad.ui.theme.WidgetGray10
-import ru.itmo.se.mad.ui.theme.WidgetGray3;
-import ru.itmo.se.mad.ui.theme.WidgetGray45
 import ru.itmo.se.mad.ui.theme.WidgetGray5
 import ru.itmo.se.mad.ui.theme.WidgetGray70
 import ru.itmo.se.mad.ui.theme.WidgetGray80
